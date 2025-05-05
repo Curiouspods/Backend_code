a4d93229a3373ea1406c52c7dd1c9b082756b235
Vtex.ai <Curiouspods@gmail.com>
2025-05-05 13:26:42 +0530

    Update index.js

--- Services/auth/index.js
+++ Services/auth/index.js
@@ -27,14 +27,14 @@ app.get("/", (req, res) => {
 });
 // Middleware
 app.use(express.json());
-app.use(cors(
-    {
-        origin: 'https://vtex-ai.vercel.app/','http://localhost:3000',  
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
-        credentials: true 
-    }
-));
+app.use(cors({
+    origin: (origin, callback) => {
+        callback(null, true); // Allow all origins
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
 app.use(helmet());
 app.use(session({
     secret: process.env.SESSION_SECRET || 'yourSecretKey',
